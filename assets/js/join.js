76874550f85785bba7f4aeb45c8931b6fb9e2a0a
ThayalanGR <grthayalan18@gmail.com>
2018-12-10 22:08:35 +0530

    touch sensitive bug fixed

--- assets/js/join.js
+++ assets/js/join.js
@@ -1,47 +1,10 @@
-function _(element) {
-    return document.getElementById(element)
-  }
-
-
-  
-_('leave-message').addEventListener("submit", event => {
-    _("leave-button").setAttribute("disabled", true)
-    _("leave-button").innerHTML = `<i class="fas fa-spinner fa-spin text-whitesa"></i>&nbsp;submitting`
-    event.preventDefault()
-    console.log(event)
-    console.log(event.target.elements[0].value)
-    let name = event.target.elements["name"].value
-    let email = event.target.elements["email"].value
-    let subject = event.target.elements["subject"].value
-    let message = event.target.elements["leave-message"].value
-  
-    console.log(name, email, subject, message)
-  
-    const apiUrl = `../../restapi/leavemessage.php?name=${name}&email=${email}&subject=${organisation}&contact=${contact}&location=${location}&message=${message}`
-    console.log(apiUrl)
-    fetch(apiUrl)
-      .then(resp => resp.json())
-      .then(response => {
-        console.log(response)
-        if (response.status) {
-          _("leave-button").classList.remove("btn-primary")
-          _("leave-button").classList.add("btn-success")
-          _("leave-button").innerHTML = `submitted`
-          setTimeout(function () {
-            _("leave-message").reset()
-            _("leave-button").removeAttribute("disabled")
-            _("leave-button").innerHTML = `submit`
-            _("leave-button").classList.remove("btn-success")
-            _("leave-button").classList.add("btn-primary")
-          }, 2000)
-        }
-      })
-      .catch(error => console.log(error))
-  
-  })
 
 
   //career form handler
+  function _(element) {
+    return document.getElementById(element)
+  }
+
 
 _('career-form').addEventListener("submit", event => {
     _("career-button").setAttribute("disabled", true)
@@ -92,10 +55,11 @@ _('career-form').addEventListener("submit", event => {
             _("career-button").innerHTML = `submit`
             _("career-button").classList.remove("btn-success")
             _("career-button").classList.add("btn-primary")
-            $("#modalContactForm").modal("hide")
+            // $("#modalContactForm").modal("hide")
           }, 3000)
         }
       })
       .catch(error => console.log(error))
   
   })
+
---
+++ assets/js/leave.js
@@ -0,0 +1,41 @@
+function _(element) {
+  return document.getElementById(element)
+}
+
+
+
+_('leave-message').addEventListener("submit", event => {
+  _("leave-button").setAttribute("disabled", true)
+  _("leave-button").innerHTML = `<i class="fas fa-spinner fa-spin text-whitesa"></i>&nbsp;submitting`
+  event.preventDefault()
+  console.log(event)
+  console.log(event.target.elements[0].value)
+  let name = event.target.elements["name"].value
+  let email = event.target.elements["email"].value
+  let subject = event.target.elements["subject"].value
+  let message = event.target.elements["message"].value
+
+  console.log(name, email, subject, message)
+
+  const apiUrl = `../../restapi/leavemessage.php?name=${name}&email=${email}&subject=${subject}&message=${message}`
+  console.log(apiUrl)
+  fetch(apiUrl)
+    .then(resp => resp.json())
+    .then(response => {
+      console.log(response)
+      if (response.status) {
+        _("leave-button").classList.remove("btn-primary")
+        _("leave-button").classList.add("btn-success")
+        _("leave-button").innerHTML = `submitted`
+        setTimeout(function () {
+          _("leave-message").reset()
+          _("leave-button").removeAttribute("disabled")
+          _("leave-button").innerHTML = `submit`
+          _("leave-button").classList.remove("btn-success")
+          _("leave-button").classList.add("btn-primary")
+        }, 2000)
+      }
+    })
+    .catch(error => console.log(error))
+
+})
